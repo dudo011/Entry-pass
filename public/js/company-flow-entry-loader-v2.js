04eb4f4e6386a408122422c524df4fa394f0acc0
Mr.kkim <dh.kkim0104@gmail.com>
2026-08-08 20:15:37 +0900

    기사 링크와 업체 일반 화면 진입 스크립트 분리

---
+++ public/js/company-flow-entry-loader-v2.js
@@ -0,0 +1,12 @@
+(() => {
+  const hash = new URLSearchParams(String(location.hash || '').replace(/^#/, ''));
+  const query = new URLSearchParams(location.search);
+  const driverToken = hash.get('driverAccess') || query.get('driverAccess');
+
+  if (driverToken) {
+    document.write('<scr' + 'ipt src="/js/driver-access-paged-v2.js?v=20260808-001"></scr' + 'ipt>');
+    return;
+  }
+
+  document.write('<scr' + 'ipt src="/js/company-flow-v1.js?v=20260808-001"></scr' + 'ipt>');
+})();
